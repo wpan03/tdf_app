d6ad709063f7343f5c687f01f89c95dfbdc03f42
Martin Pan <wpan03@email.wm.edu>
2020-05-17 21:21:28 -0400

    format serveral files

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
 import base64
 import webbrowser
 from PIL import Image
@@ -14,33 +14,33 @@ from find_repeat import find_repeat
 
 selectbox = st.sidebar.selectbox(
     'What do you want to do?',
-    ('Home Page','Transfer OCP Created Project', 'Transfer OCP Amended Project', 
-    'Search Duplicate','Merge', 'Logical Consistency Check', 'Find Repeat')
+    ('Home Page', 'Transfer OCP Created Project', 'Transfer OCP Amended Project',
+     'Search Duplicate', 'Merge', 'Logical Consistency Check', 'Find Repeat')
 )
 
 st.sidebar.info("View [source code](https://github.com/wpan03/tdf_app)")
 
 if selectbox == 'Home Page':
-  st.title("Welcome！")
-  st.markdown("This website contains some useful techniques for TUFF's work. ")
-  st.markdown('**Please choose what you want to do in the side bar.**')
-  image = Image.open('sunrise.png')
-  st.image(image, use_column_width=True)
+    st.title("Welcome！")
+    st.markdown("This website contains some useful techniques for TUFF's work. ")
+    st.markdown('**Please choose what you want to do in the side bar.**')
+    image = Image.open('sunrise.png')
+    st.image(image, use_column_width=True)
 
 elif selectbox == 'Transfer OCP Created Project':
-  ocp_transfer_created()
-  
+    ocp_transfer_created()
+
 elif selectbox == "Transfer OCP Amended Project":
-  transfer_amend()
+    transfer_amend()
 
 elif selectbox == 'Search Duplicate':
-  search_duplicate()
+    search_duplicate()
 
 elif selectbox == "Merge":
-  merge()
+    merge()
 
 elif selectbox == 'Logical Consistency Check':
     examine()
 
 elif selectbox == 'Find Repeat':
-  find_repeat()
+    find_repeat()
--- merge.py
+++ merge.py
@@ -2,14 +2,17 @@ import pandas as pd
 import streamlit as st
 import base64
 
+
 def merge():
     st.header('Merge according to project id')
     export_file = st.file_uploader("Choose an exported csv file", type="csv")
-    stage2_file = st.file_uploader('Choose the stage 2 / qa excel file', type = 'xlsx')
-    
+    stage2_file = st.file_uploader(
+        'Choose the stage 2 / qa excel file', type='xlsx')
+
     if (export_file != None) and (stage2_file != None):
         stage2_sheet = pd.ExcelFile(stage2_file)
-        sheet = st.selectbox('Which tab in the stage 2 / qa spreadsheet?', stage2_sheet.sheet_names)
+        sheet = st.selectbox(
+            'Which tab in the stage 2 / qa spreadsheet?', stage2_sheet.sheet_names)
         df_stage2 = pd.read_excel(stage2_file, sheet_name=sheet)
         df_export = pd.read_csv(export_file)
 
@@ -18,26 +21,26 @@ def merge():
 
         country = st.selectbox('Which country?', df_stage2['Country'].unique())
         df_stage2 = df_stage2[df_stage2['Country'] == country]
-        
-        
 
-    
         left_identifier = st.text_input('stage 2 / qa identifier', value=id)
-        right_identifier = st.text_input('export identifier', value = 'project_id')
-        how_to_merge = st.text_input('how to merge', value = 'left')
+        right_identifier = st.text_input(
+            'export identifier', value='project_id')
+        how_to_merge = st.text_input('how to merge', value='left')
 
         if st.button('merge'):
-            df_merge = pd.merge(df_stage2, df_export, left_on=left_identifier, right_on=right_identifier, how=how_to_merge)
-        
-            def get_table_download_link(df):
-                    """Generates a link allowing the data in a given panda dataframe to be downloaded
-                    in:  dataframe
-                    out: href string"""
-                    csv = df.to_csv(index=False)
-                    b64 = base64.b64encode(
-                    csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-                    return f'<a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download csv file</a>'
+            df_merge = pd.merge(df_stage2, df_export, left_on=left_identifier,
+                                right_on=right_identifier, how=how_to_merge)
 
-            selections = ['project_id','year','flow_class','Country']
+            def get_table_download_link(df):
+                """Generates a link allowing the data in a given panda dataframe to be downloaded
+                in:  dataframe
+                out: href string"""
+                csv = df.to_csv(index=False)
+                b64 = base64.b64encode(
+                    csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+                return f'<a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download csv file</a>'
+
+            selections = ['project_id', 'year', 'flow_class', 'Country']
             df_merge = df_merge[selections]
-            st.markdown(get_table_download_link(df_merge), unsafe_allow_html=True)    
+            st.markdown(get_table_download_link(
+                df_merge), unsafe_allow_html=True)
