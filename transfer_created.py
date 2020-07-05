162308cee24c0c5ca298e8ae55fd1eda8dc05914
Martin Pan <wpan03@email.wm.edu>
2020-07-05 15:47:23 -0400

    refactor the code

--- transfer_created.py
+++ transfer_created.py
@@ -3,62 +3,52 @@ import pandas as pd
 import base64
 
 
-def ocp_transfer_created():
-    st.subheader('Transfer OCP Created Project')
+def reshape_dataframe(df):
+    """
+    Only keep columns with created projects information in the dataframe
 
-    ocp_create_requirement = st.checkbox('ocp create requirment')
-    if ocp_create_requirement:
-        st.markdown(
-            "+ The tab in the excel file should have following order: General Tab, Project List, and Year Tab.")
-        st.markdown(
-            "+ There should be one line above the header in the general tab")
-        st.markdown(
-            "+ There should be no line above the header in the project list and year tab")
+    Parameters
+    ----------
+    df : pd.DataFrame
+        a dataframe with format from general tab of OCP spreadsheet 
 
-    def reshape_dataframe(df):
-        """
-        Only keep columns with created projects information in the dataframe
+    Returns
+    -------
+    df : pd.DataFrame
+    the shrinked dataframe 
+    """
+    filter_col = [col for col in df if col.startswith('Projects Created')]
+    df = df[filter_col]
+    df = df.dropna(axis=1, how='all')
+    df = df.melt().dropna(axis=0)
+    return df
 
-        Parameters
-        ----------
-        df : pd.DataFrame
-            a dataframe with format from general tab of OCP spreadsheet 
+def merge_df(all_sheet):
+    """merge the created project id in each tab"""
 
-        Returns
-        -------
-        df : pd.DataFrame
-        the shrinked dataframe 
-        """
-        filter_col = [col for col in df if col.startswith('Projects Created')]
-        df = df[filter_col]
-        df = df.dropna(axis=1, how='all')
-        df = df.melt().dropna(axis=0)
-        return df
+    store = []
+    sheets = all_sheet.sheet_names
 
-    def get_create_ocp(file, delimiter):
+    for i in range(len(sheets)):
+        if i == 0:
+            df = all_sheet.parse(i, skiprows=[0])
+            df = reshape_dataframe(df)
+            df_select = df.loc[:, ['value']].reset_index(drop=True)
+            df_select.columns = ['Projects Created']
+        elif i == 1:
+            df = all_sheet.parse(i)
+            df_select = df[['Created Project ID']]
+            df_select.columns = ['Projects Created']
+        elif i >= 2:
+            df = all_sheet.parse(i)
+            df_select = df[['Projects Created']]
 
-        # Get all the tab name in the excel sheet
-        store = []
-        all_sheet = pd.ExcelFile(file)
-        sheets = all_sheet.sheet_names
+        store.append(df_select)
+        df_merged = pd.concat(store).dropna().reset_index(drop=True) 
 
-        # merge the created project id in each tab
-        for i in range(len(sheets)):
-            if i == 0:
-                df = all_sheet.parse(i, skiprows=[0])
-                df = reshape_dataframe(df)
-                df_select = df.loc[:, ['value']].reset_index(drop=True)
-                df_select.columns = ['Projects Created']
-            elif i == 1:
-                df = all_sheet.parse(i)
-                df_select = df[['Created Project ID']]
-                df_select.columns = ['Projects Created']
-            elif i >= 2:
-                df = all_sheet.parse(i)
-                df_select = df[['Projects Created']]
+        return df_merged
 
-            store.append(df_select)
-            df = pd.concat(store).dropna().reset_index(drop=True)
+def clean_merge(df, delimiter):
 
         # break projects in one cell to one project per row
         df['Projects Created'] = df['Projects Created'].astype(str)
@@ -78,6 +68,51 @@ def ocp_transfer_created():
 
         return df_clean
 
+def get_create_ocp(file, delimiter):
+
+    all_sheet = pd.ExcelFile(file)
+    df_merged = merge_df(all_sheet)
+    df_clean = clean_merge(df_merged, delimiter)
+
+    return df_clean
+
+def make_page_title():
+
+    st.subheader('Transfer OCP Created Project')
+
+    ocp_create_requirement = st.checkbox('ocp create requirment')
+    if ocp_create_requirement:
+        st.markdown(
+            "+ The tab in the excel file should have following order: General Tab, Project List, and Year Tab.")
+        st.markdown(
+            "+ There should be one line above the header in the general tab")
+        st.markdown(
+            "+ There should be no line above the header in the project list and year tab")
+
+def break_line_area(delimiter):
+
+    st.subheader('Break text to different line')
+    text_input = st.text_input('text')
+    text_input_list = text_input.split(delimiter)
+    if st.button('Split!'):
+        for i in text_input_list:
+            st.write(i)
+
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string"""
+
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(
+        csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    return f'<a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download csv file</a>'
+
+
+def ocp_transfer_created():
+    
+    make_page_title()
+
     uploaded_file = st.file_uploader(
         "Choose a the stage 1 excel file", type="xlsx")
     country = st.text_input('Country')
@@ -89,23 +124,9 @@ def ocp_transfer_created():
         df_ocp['country'] = country
         df_ocp = df_ocp[['source', 'country', 'Projects Created']]
 
-        def get_table_download_link(df):
-            """Generates a link allowing the data in a given panda dataframe to be downloaded
-            in:  dataframe
-            out: href string"""
-
-            csv = df.to_csv(index=False)
-            b64 = base64.b64encode(
-                csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-            return f'<a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download csv file</a>'
 
         st.markdown(get_table_download_link(df_ocp), unsafe_allow_html=True)
         st.text('Congratulations!')
         st.text("You transferred about {} projects".format(df_ocp.shape[0]))
 
-    st.subheader('Break text to different line')
-    text_input = st.text_input('text')
-    text_input_list = text_input.split(delimiter)
-    if st.button('Split!'):
-        for i in text_input_list:
-            st.write(i)
+    break_line_area(delimiter)
