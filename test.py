bbb174c214b3654b76c3ae5fc4312a17b0300c38
Martin Pan <wpan03@email.wm.edu>
2020-09-01 15:23:03 -0400

    add unit test for transfer amend

--- test.py
+++ test.py
@@ -1,14 +1,18 @@
 import pandas as pd
 from transfer_created import get_create_ocp
+from transfer_amended import get_amend
 
 stage1 = pd.ExcelFile('test_data/stage1.xlsx')
 
 
-def test_transfer_created(excel_file):
+def test_transfer(excel_file):
     df_created = get_create_ocp(excel_file, delimiter=',', update_2018=True)
+    df_amend = get_amend(stage1, delimiter=',', update_2018=True)
+
     assert df_created.shape[0] == 15
+    assert df_amend.shape[0] == 86
 
 
-test_transfer_created(stage1)
+test_transfer(stage1)
 
 print('all test passed')
